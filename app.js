f7193f4ef3be34091aff885c1b8a115a54fa39fd
Glory Agatevure <agatevureglory@gmail.com>
2021-09-06 17:27:59 +0100

    Removed some files

--- app.js
+++ app.js
@@ -1,87 +1,48 @@
 require('dotenv').config()
 var Twitter = require('twitter');
-
 require('dotenv').config()
 const Twit = require("twit")
 const config = require("./config")
 const T = new Twit(config)
-const request = require('request');
 const axios = require("axios")
 
-var randomWordURL = "http://api.wordnik.com/v4/words.json/randomWord?" +
-                    "&minLength=5&maxLength=-1" +
-                    "&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
 
-//Start once
-// tweeter();
-console.log("starting the bot")
-// Once every N milliseconds
-  // setInterval(tweeter, 1000);
-
-// // Here is the bot!
-// function tweeter() {
-
-//   request(randomWordURL, gotData);
-
-//   function gotData(error, response, body) {
-//     if (!error) {
-//       var data = JSON.parse(body);
-
-//       // This is a random number bot
-//       var tweet = 'The word of the day is: ' + data.text;
-
-//       // Post that tweet!
-//       T.post('statuses/update', { status: tweet }, tweeted);
-
-//       // Callback for when the tweet is sent
-//       function tweeted(err, data, response) {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           console.log('Success: ' + data.text);
-//           console.log("tweeted")
-//         }
-//       }
-//     }
-//   }
-//   }
 // Endpoint to get all protocols
-// axios.get('https://api.boardroom.info/v1/protocols')
-//   .then(response => {
-//     console.log(response.data.data);
-//     response.data.data.forEach(item => {
-//       // console.log('Cname: ' + item.cname)
-//     })
-//   }, error => {
-//   // handle the response error
-//   return Promise.reject(error);
-// });
+axios.get('https://api.boardroom.info/v1/protocols')
+  .then(response => {
+    console.log(response.data.data);
+    response.data.data.forEach(item => {
+      // console.log('Cname: ' + item.cname)
+    })
+  }, error => {
+  // handle the response error
+  return Promise.reject(error);
+});
 
 // // Endpoint to get all voters from a protocol
 
-// console.log("starting..")
-// async function getProtocol() {
-//   const res = await axios.get(`https://api.boardroom.info/v1/protocols/aave/voters`)
-//   // console.log(res.data.data)
-//   res.data.data.forEach(item =>{
-//     item.protocols.forEach(protocol =>{
-//       console.log(protocol.protocol)
-//     })
-//   })
-// }
+async function getProtocol() {
+  const res = await axios.get(`https://api.boardroom.info/v1/protocols/aave/voters`)
+  // console.log(res.data.data)
+  res.data.data.forEach(item =>{
+    item.protocols.forEach(protocol =>{
+      console.log(protocol.protocol)
+    })
+  })
+}
 
 // getProtocol()
 
-// axios.get(`https://api.boardroom.info/v1/protocols/aave/voters`)
-//   .then(response => {
-//     console.log(response.data.data);
-//     response.data.data.forEach(item => {
-//       console.log('Address: ' + item.address + ' \n total vote cast ' + item.totalVotesCast)
-//     })
-//   }, error => {
-//   // handle the response error
-//   return Promise.reject(error);
-// });
+axios.get(`https://api.boardroom.info/v1/protocols/aave/voters`)
+  .then(response => {
+    console.log(response.data.data);
+    response.data.data.forEach(item => {
+      console.log('Address: ' + item.address + ' \n total vote cast ' + item.totalVotesCast)
+    })
+  }, error => {
+  // handle the response error
+  return Promise.reject(error);
+});
 
 let startDateResult, endDateResult = ""
 //Get all proposals, their start and endtime
@@ -138,15 +99,15 @@ getProposals()
 // console.log(getAllVoterFromAProtocol)
 
 // execute simultaneous requests 
-// axios.all([
-//   axios.get('https://api.github.com/users/mapbox'),
-//   axios.get('https://api.github.com/users/phantomjs')
-// ])
-// .then(responseArr => {
-//   //this will be executed only when all requests are complete
-//   console.log('Date created: ', responseArr[0].data.created_at);
-//   console.log('Date created: ', responseArr[1].data.created_at);
-// });
+axios.all([
+  axios.get('https://api.github.com/users/mapbox'),
+  axios.get('https://api.github.com/users/phantomjs')
+])
+.then(responseArr => {
+  //this will be executed only when all requests are complete
+  console.log('Date created: ', responseArr[0].data.created_at);
+  console.log('Date created: ', responseArr[1].data.created_at);
+});
 
 
 
--- config.js
+++ config.js
@@ -1,16 +1,3 @@
-require('dotenv').config()
-
-// const Twit = require('twit')
-
-// const T = new Twit({
-//     consumer_key: process.env.TWITTER_API_KEY,
-//     consumer_secret : process.env.TWITTER_API_SECRET,
-//     access_token : process.env.TWITTER_ACCESS_TOKEN,
-//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// }) 
-
-// module.exports = T
-
 module.exports = {
     consumer_key: process.env.TWITTER_API_KEY,
     consumer_secret: process.env.TWITTER_API_SECRET,
